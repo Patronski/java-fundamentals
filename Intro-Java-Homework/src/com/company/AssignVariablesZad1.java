b9a7fa89f9cf425fedf9a4675d1d0dfae9ab8900
Doncho Patronski <donstz@yahoo.com>
2016-03-20 17:17:05 +0200

    uploading homework fo Java

---
+++ Intro-Java-Homework/src/com/company/AssignVariablesZad1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class AssignVariablesZad1 {
+
+    public static void main(String[] args) {
+        boolean bVariable = false;
+        String strVariable = "Palo Alto, CA";
+        short sNum = 32767;
+        int iNum = 2000000000;
+        double dNum = 0.1234567891011;
+        float fNum = 0.5f;
+        long lNum = 919827112351L;
+        byte smallestNum = 127;
+        char sign = 'c';
+
+//        short whatHappens = 32768;
+    }
+}
